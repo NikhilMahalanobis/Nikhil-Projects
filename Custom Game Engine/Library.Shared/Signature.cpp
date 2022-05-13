329214072ee86ff6a84f391cc8d45025ed67cdbd
NikhilMahalanobis <n1xsddd@gmail.com>
2022-05-13 11:34:19 -0400

    Delete Custom Game Engine/Library.Shared directory

--- Custom Game Engine/Library.Shared/Signature.cpp
+++
@@ -1,9 +0,0 @@
-#include "Signature.h"
-
-namespace FieaGameEngine
-{
-	Signature::Signature(const std::string& name, DatumTypes type, size_t numElements, size_t offset) :
-		_name(name), _type(type), _numElements(numElements), _offset(offset)
-	{
-	}
-}
--- Custom Game Engine/Library.Shared/pch.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include<cstddef>
-#include<cstdint>
-#include<cassert>
-#include<stdexcept>
-#include<functional>
-#include<initializer_list>
